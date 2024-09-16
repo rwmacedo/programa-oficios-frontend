84b62c8733e8b34f12f053b63f47101972751580
Renata Werneck <rwmacedo@gmail.com>
2024-09-16 06:46:59 -0300

    ajustando visualização de arquivo32

--- src/app/components/oficio-pdf-viewer.component.ts
+++ src/app/components/oficio-pdf-viewer.component.ts
@@ -24,17 +24,12 @@ export class OficioPdfViewerComponent implements OnInit {
 
   loadPdf(fileName: string) {
     this.oficioService.getPdfUrl(fileName).subscribe((blob: Blob) => {
-      // Verifique se o Blob é do tipo PDF
-      if (blob.type === 'application/pdf') {
+      
         const fileURL = URL.createObjectURL(blob);
         this.pdfUrl = fileURL;
-      } else {
-        console.error('O arquivo retornado não é um PDF');
-      }
-      this.isLoading = false;  // Quando o PDF for carregado, pare de mostrar o status de carregamento
-    }, error => {
-      console.error('Erro ao carregar o PDF', error);
-      this.isLoading = false;  // Pare de mostrar o status de carregamento em caso de erro
-    });
-  }
+       this.isLoading = false;  // Quando o PDF for carregado, pare de mostrar o status de carregamento
+   
+    }
+  )}
 }
+
